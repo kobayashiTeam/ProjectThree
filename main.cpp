1ae6f45d6ae59163a6d344d2fa609116582468c6
kobayashiTeam <marimo123@yahoo.ne.jp>
2026-06-17 08:36:15 +0900

    what

--- main.cpp
+++ main.cpp
@@ -204,8 +204,8 @@ void UpdateScene()
     g_Time += 0.016f;  // ≈60FPS
 
     // カメラ（自由に動かしたい場合は後でInput対応）
-    XMVECTOR eye = XMVectorSet(0.0f, 2.0f, -5.0f, 0.0f);//0,2,-5
-    XMVECTOR at = XMVectorSet(0.0f, 5.0f, 0.0f, 0.0f);//up0,10,0にすると謎の赤い模様が見える
+    XMVECTOR eye = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);//0,2,-5
+    XMVECTOR at = XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f);//up0,10,0にすると謎の赤い模様が見える
     XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
     g_pCamera->Update(eye, at, up);
 
@@ -224,8 +224,8 @@ void Render()
     g_pRenderer->BeginFrame(g_pCamera, 0.1f, 0.12f, 0.15f, 1.0f);
 
     // 3. モデルの登録（距離計算はRendererが裏で自動でやってくれる）
-    g_pRenderer->Submit(g_pMainModel, RenderPass::Opaque,BlendMode::Opaque);
-    g_pRenderer->Submit(g_pMainModel2, RenderPass::Transparent,BlendMode::AlphaBlend);
+    //g_pRenderer->Submit(g_pMainModel, RenderPass::Opaque,BlendMode::Opaque);
+    //g_pRenderer->Submit(g_pMainModel2, RenderPass::Transparent,BlendMode::AlphaBlend);
 
     // 4. レンダーキューの実行（適切なステートで一括描画）
     g_pRenderer->Execute();
