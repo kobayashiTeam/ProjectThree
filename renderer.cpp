daf74aa1459707b1bd84a5c4a29984b13326d235
kobayashiTeam <marimo123@yahoo.ne.jp>
2026-06-17 20:25:58 +0900

    lose this time

--- renderer.cpp
+++ renderer.cpp
@@ -238,7 +238,7 @@ void Renderer::Execute()
     // ─── 【新設】スカイボックスの描画 ───
     if (m_pSkyBox) {
         // 境目でステートをスカイボックス用に切り替える！front,depthlessequal
-        m_rasterStates->Bind(pContext, RasterizerStates::CullMode::None);       // 内側を見せるため前面カリング
+        m_rasterStates->Bind(pContext, RasterizerStates::CullMode::Back);       // 内側を見せるため前面カリング
         m_dsStates->Bind(pContext, DepthStencilStates::Mode::DepthLessEqual);    // 1.0の隙間に滑り込ませる
 
         // 描画実行
